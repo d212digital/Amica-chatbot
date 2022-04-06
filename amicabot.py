5d702b8460048afbc0dc4473b1f2e4f8b2f508dd
David Adamson <david@212digital.co.uk>
2022-04-06 14:17:56 +0100

    adding python version for render

--- amicabot.py
+++ amicabot.py
@@ -5,6 +5,8 @@ from flask import Flask, request
 import os
 import openai
 
+python_version = os.getenv('PYTHON_VERSION')
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
 completion = openai.Completion()
 
